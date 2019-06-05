2ea1d282c483f2b62295fedcb0427d82f1041330
Jeekc0308 <jeekc0308@gmail.com>
2019-06-05 20:37:30 +0900

    navigation

--- src/navigation.js
+++ src/navigation.js
@@ -1,6 +1,7 @@
 let nav;
 (() => {
     nav = document.querySelector("#navigation");
+    
     function scrollNavigation() {
         const y = window.scrollY;
         
@@ -13,6 +14,23 @@ let nav;
 
     scrollNavigation();
     window.addEventListener('scroll', scrollNavigation);
+
+    const menu = document.getElementById('menu');
+    const background = document.getElementById('background');
+
+    document.getElementById("menu-opener").addEventListener('click', e => {
+        e.preventDefault();
+        background.classList.add('show');
+        menu.classList.add('show');
+    });
+    const disappearListener = e => {
+        background.classList.remove('show');
+        menu.classList.remove('show');
+    };
+    background.addEventListener('click', disappearListener);
+    for (const i of menu.querySelectorAll('a[data-to]')) {
+        i.addEventListener('click', disappearListener);
+    }
 })();
 
 export default nav;
