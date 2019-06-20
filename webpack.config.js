c5f16c60b50058c04c712f8e03f3d525b9345a83
Doyeong Kim <jeekc0308@gmail.com>
2019-06-20 14:14:15 +0900

    .

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,21 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    entry: path.join(__dirname, '/src/index.js'),
-    output: {
-        path: path.join(__dirname, '/dist'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [
-            {test: /\.js/i, exclude: /node_modules/, loader: 'babel-loader'},
-            {test: /\.css/i, use: ['style-loader', 'css-loader']}
-        ]
-    },
-    devServer: {
-        contentBase: path.join(__dirname, 'dist'),
-        compress: true,
-        port: 9000
-    }
-}
+  entry: path.join(__dirname, "/src/index.js"),
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      { test: /\.js$/i, exclude: /node_modules/, loader: "babel-loader" },
+      { test: /\.css$/i, use: ["style-loader", "css-loader"] },
+      { test: /\.json$/i, loader: "json-loader" }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000
+  }
+};
